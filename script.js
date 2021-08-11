1b83e524a482bd34c37f6219994b9c3773b9297d
Josh <zylo.codes@gmail.com>
2021-08-11 12:59:08 -0500

    multiple instances

--- script.js
+++ script.js
@@ -15,7 +15,46 @@ const createScene = function() {
   const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(0, 2, 0))
   
   const ground = buildGround()
-  const house = buildHouse(2)
+  
+  const detached_house = buildHouse(1)
+  detached_house.rotation.y = -Math.PI / 16
+  detached_house.position.x = -6.8
+  detached_house.position.z = 2.5
+
+  const semi_house = buildHouse(2)
+  semi_house.rotation.y = -Math.PI / 16
+  semi_house.position.x = -4.5
+  semi_house.position.z = 3
+
+  const places = [
+    { houseType: 1, rotation: -Math.PI / 16, x:  -6.8, z: 2.5 },
+    { houseType: 2, rotation: -Math.PI / 16, x: -4.5, z: 3 },
+    { houseType: 2, rotation: -Math.PI / 16, x: -1.5, z: 4 },
+    { houseType: 2, rotation: -Math.PI / 3, x: 1.5, z: 6 },
+    { houseType: 2, rotation: 15 * Math.PI / 16, x: -6.4, z: -1.5 },
+    { houseType: 1, rotation: 15 * Math.PI / 16, x: -4.1, z: -1 },
+    { houseType: 2, rotation: 15 * Math.PI / 16, x: -2.1, z: -0.5 },
+    { houseType: 1, rotation: 5 * Math.PI / 4, x: 0, z: -1 },
+    { houseType: 1, rotation: Math.PI + Math.PI / 2.5, x: 0.5, z: -3 },
+    { houseType: 2, rotation: Math.PI + Math.PI / 2.1, x: 0.75, z: -5 },
+    { houseType: 1, rotation: Math.PI + Math.PI / 2.25, x: 0.75, z: -7 },
+    { houseType: 2, rotation: Math.PI / 1.9, x: 4.75, z: -1 },
+    { houseType: 1, rotation: Math.PI / 1.95, x: 4.5, z: -3 },
+    { houseType: 2, rotation: Math.PI / 1.9, x: 4.75, z: -5 },
+    { houseType: 1, rotation: Math.PI / 1.9,x:  4.75, z: -7 },
+    { houseType: 2, rotation: -Math.PI / 3, x: 5.25, z: 2 },
+    { houseType: 1, rotation: -Math.PI / 3, x: 6, z: 4 },
+  ]
+
+  const houses = places.map(({ houseType, rotation, x, z }, i) => {
+    const id = `house${i}`
+    const house = houseType === 1 
+      ? detached_house.createInstance(id)
+      : semi_house.createInstance(id)
+    house.rotation.y = rotation
+    house.position.x = x
+    house.position.z = z
+  })
 
   return scene
 }
@@ -67,7 +106,7 @@ const buildRoof = (width = 1) => {
 }
 
 const buildGround = () => {
-  const ground = BABYLON.MeshBuilder.CreateGround('ground', { width: 10, height: 10 })
+  const ground = BABYLON.MeshBuilder.CreateGround('ground', { width: 15, height: 16 })
   const groundMat = new BABYLON.StandardMaterial('groundMat')
   groundMat.diffuseColor = new BABYLON.Color3(0, 1, 0)
   ground.material = groundMat
